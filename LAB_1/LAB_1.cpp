dede4edd08ab107d32ed50576aebb5e918137698
Goodjop <sashakinser2005@mail.ru>
2023-11-28 05:19:53 +0400

    не компланарны

--- LAB_1/LAB_1.cpp
+++ LAB_1/LAB_1.cpp
@@ -7,9 +7,9 @@ int main() {
 	setlocale(LC_ALL, "rus");
 	Matrix<complex<double>> a1(3, 3, complex<double>(1.0, 1.0));
 	Matrix<int>a2(3, 3, 4, 1);
-	/*Matrix<complex<double>> b2(1, 3, complex<double>(2.0, 2.0));
-	Matrix<complex<double>> c3(1, 3, complex<double>(3.0, 3.0));*/
-	cout << a2<<"\n";
+	Matrix<complex<double>> b2(1, 3, complex<double>(2.0, 2.0));
+	Matrix<complex<double>> c3(1, 3, complex<double>(3.0, 3.0));
+	cout << a2 << "\n";
 	cout << a2.trace()<<"\n" << "\n";
 	Matrix<float>a(1, 3, 1.2);
 	Matrix<float>b(1, 3, 2.4);
@@ -17,10 +17,10 @@ int main() {
 	cout << a;
 	cout << b;
 	cout << c<< "\n";
-	cout<<a1.trace()<< "\n" << "\n";
-	bool coplanar = checking_for_coplanarity(a, b, c);
+	cout <<a1.trace()<< "\n" << "\n";
+	bool coplanar = checking_for_coplanarity(a1, b2, c3);
 	cout << coplanar;
 	a = a * 2.0f;
-	a = 2.0f *a ;
+	a = 2.0f * a ;
 	return 0;
 }
